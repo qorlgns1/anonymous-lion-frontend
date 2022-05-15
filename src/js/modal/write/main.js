d80d905eadcda1680a6cef24722f3baebb418d6a
qorlgns1 <rlgns0610@gmail.com>
2022-05-16 00:41:28 +0900

    Feat: 추가 서버에 게시물 전송시 작성한 글 초기화와 모달 닫는 시점 변경
    모달 닫을 때 fetch 한 후 모달을 닫으면 너무 늦게 모달이 닫혀서 모달 닫는 시점을 fetch 보내기 이전으로 변경

--- src/js/modal/write/main.js
+++ src/js/modal/write/main.js
@@ -3,6 +3,10 @@ import { loadBoardData } from "../../api/loadBoardData.js";
 import { toggleModal } from "../common.js";
 import { nicknameCheck } from "../nickname/main.js";
 
+const resetWriteBox = (textarea) => {
+  textarea.value = "";
+};
+
 export const writeModal = () => {
   const targetModal = document.querySelector(".modal-write");
   const showWriteModalBtn = document.querySelector(".add");
@@ -31,9 +35,13 @@ export const writeModal = () => {
       description: textBox.value,
     };
 
+    // 서버에 POST 요청시 처리 속도가 느려서 먼저 텍스트박스를 지우고 모달을 닫기위해
+    // fetch 안에 안넣고 위로 올림
+    resetWriteBox(textBox);
+    toggleModal(targetModal);
+
     fetch(serverURL, createSendData("POST", sendData)).then(() => {
       loadBoardData();
-      toggleModal(targetModal);
     });
   });
 };
